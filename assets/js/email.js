cecdcabcb5fc41d5400fab7b66cf0120edc4b28d
dkidanu1 <dkidanu@gmail.com>
2021-03-21 05:33:17 +0000

    Remove console log

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,6 @@
 
 function sendMail(contactForm) {
-    console.log("Hello", contactForm);
+    ("Hello", contactForm);
     emailjs.send("gmail", "greenpark", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.email.value,
@@ -8,13 +8,13 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response.status, response.text);
+            ("SUCCESS", response.status, response.text);
             alert("Thank you for submitting. We will take care of you as soon as possible.");
             contactForm.reset();
         },
        
         function(error) {
-            console.log("Something went wrong with your request. Please try again", error);
+            ("Something went wrong with your request. Please try again", error);
         },
        
     );
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -5,16 +5,16 @@ var quotes = new Array();
     quotes[2] = "I was on my gap year travelling East Africa, I came across Green Park and it was a diamond in the rough, fantastic experience";
     quotes[3] = "Love the convenience of getting to Green Park and the variety of activities, staff is also very helpful";
     quotes[4] = "I would live there if I could! the tranquility and action are balanced very well";
-console.log(quotes);
+(quotes);
 var counter = 0;
 
 function loop() {
     if (counter > 4) counter = 0;
-    console.log("quote: ", quotes[counter]);
+    ("quote: ", quotes[counter]);
     const testimonal = document.getElementById('textslide');
     testimonal.innerText = '"' +  quotes[counter] + '"';
     counter++;
-    console.log(counter);
+    (counter);
     setTimeout(loop, 5000);
 }
 loop();
